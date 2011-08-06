556336fd86f5b9f53edf82de8c5a9634f19e0a59
Alexander Zhukov <outsideroff@gmail.com>
2011-08-06 16:21:52 +0400

    hello world

---
+++ src/webzavod1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : webzavod1.cpp
+// Author      : Alexander Zhukov
+// Version     :
+// Copyright   : MIT license
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "start" << endl; // prints start
+	return 0;
+}
